04c1e6fe0d2307a366eaa3eca8916a924c62f9ac
Cristiano912 <142909694+Cristiano912@users.noreply.github.com>
2023-09-06 15:51:51 +0000

    wow

--- printPrac/printingPractice.java
+++ printPrac/printingPractice.java
@@ -2,8 +2,15 @@ class Cristiano
 {
     public static void main(String args[])
     {
-        System.out.print("text");
-        System.out.print(" ");
-        System.out.print("text");
+        System.out.print("  ");
+        System.out.print("Welcome ");
+        System.out.print("  ");
+        System.out.println("");
+        System.out.print("User Name:Cristiano Castaneda");
+        System.out.println("");
+        System.out.print("User Age:14");
+        System.out.println("");
+        System.out.print("Thanks for playing!");
+       
     }
 }
