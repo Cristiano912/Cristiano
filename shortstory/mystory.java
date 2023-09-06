8c68228b9ce5220b7ccae0da301b614481a125b9
Cristiano912 <142909694+Cristiano912@users.noreply.github.com>
2023-09-06 15:33:24 +0000

    this is my message

---
+++ shortstory/mystory.java
@@ -0,0 +1,12 @@
+class secondpractice
+{
+    public static void main(String args[])
+    {
+        System.out.print("This is the story of a legendary chihuaha named Steve. Steve was 
+        an ancient dog that lived with people in egypt. He protected them from harmfull animals
+         and creatures such as racooons, snakes and even hippos. The people of egpyt loved having
+          this dog aound them at all times. ");
+        
+
+    }
+}
