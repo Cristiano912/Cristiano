82a0e55103b3644fc308eb2395bd1ab95e13cc54
Cristiano912 <142909694+Cristiano912@users.noreply.github.com>
2023-09-06 16:24:59 +0000

    i

--- shortstory/mystory.java
+++ shortstory/mystory.java
@@ -2,10 +2,17 @@ class secondpractice
 {
     public static void main(String args[])
     {
-        System.out.print("This is the story of a legendary chihuaha named Steve. Steve was 
+        System.out.println("This is the story of a legendary chihuaha named Steve. Steve was 
         an ancient dog that lived with people in egypt. He protected them from harmfull animals
-         and creatures such as racooons, snakes and even hippos. The people of egpyt loved having
-          this dog aound them at all times. ");
+        and creatures such as racooons, snakes and even hippos. The people of egpyt loved having
+        this dog aound them at all times. One day there was a king cobra that tried to attack the
+        village people. ");
+        System.out.println("the king cobra striked at one person but out of nowhere Steve jumped in
+        and took the hit. Steve was struck but started fighting the snake anyways. Steve bit the
+        snake and started wipping it aound. eventually the snake died from the trauma. Steve sat
+        down and slowley died from the venom. The people of Egypt respected the dog and they even
+         made a statue in his honer for saving the village ");
+
         
 
     }
