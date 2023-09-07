cbf00ce3b5064702d7aa486aefcf3c5f88027be9
Cristiano912 <142909694+Cristiano912@users.noreply.github.com>
2023-09-07 13:59:53 +0000

    yippe

--- madLibs/CristianomadLibs.java
+++ madLibs/CristianomadLibs.java
@@ -2,10 +2,26 @@ class CristianomadLibs
 {
     public static void main(String args[])
     {
-        System.out.println("Booty, ship, hat, feather, wheel, parrot, hook, sword, sea, crew");
-        System.out.println("There once was a pirate that lost all his-------at------.
-        He ate his------ then hit his------. The pirate spun the------- with his------.
-        He threw his----- on the table and the------- came off. He swung his ------ and
-        hit the ------ making him sink.");
+        String noun1 = "treasure";
+        String noun2 = "ship";
+        String noun3 = "hat";
+        String noun4 = "feather";
+        String verb = "swung";
+        String noun5 = "parrot";
+        String noun6 = "sea";
+        String noun7 = "sword";
+        String adjective = "fast";
+        String noun8 = "hook";
+
+        System.out.println("There once was a pirate that lost all his " + noun1);
+        System.out.println("He ate his " + noun5);
+        System.out.println("he " + verb);
+        System.out.println("his " + noun7);
+        System.out.println("He threw his " + noun3);
+        System.out.println("this broke the " + noun4);
+        System.out.println("he smashed his " + noun8);
+        System.out.println("he ran as " + adjective);
+        System.out.println("as he could off " + noun2);
+        System.out.println("he fell into the " + noun6);
     }
 }   
