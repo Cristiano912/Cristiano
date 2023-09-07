0227211f7cc9a0b2292e7cd66fd2fb3e5a0725b8
Cristiano912 <142909694+Cristiano912@users.noreply.github.com>
2023-09-07 02:58:17 +0000

    h

---
+++ madLibs/CristianomadLibs.java
@@ -0,0 +1,11 @@
+class CristianomadLibs
+{
+    public static void main(String args[])
+    {
+        System.out.println("Booty, ship, hat, feather, wheel, parrot, hook, sword, sea, crew");
+        System.out.println("There once was a pirate that lost all his-------at------.
+         He ate his------ then hit his------. the pirate spun the------- with his------.
+         he threw his----- on the table and the------- came off. he swung his ------ and
+          hit the ------ making him sink.");
+    }
+}   
