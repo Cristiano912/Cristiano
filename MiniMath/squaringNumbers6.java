b7ea301f3e812a9f4e1f676502f61610072cc44b
Cristiano912 <142909694+Cristiano912@users.noreply.github.com>
2023-09-14 14:19:29 +0000

    w

---
+++ MiniMath/squaringNumbers6.java
@@ -0,0 +1,13 @@
+class squaringNumbers6
+{
+    public static void main(String args[])
+    {
+        byte number1 = 3;
+        byte number2 = 3;
+        byte number3 = 3;
+        byte number4 = 3;
+
+
+        System.out.println(number1 * number2 * number3 * number4);
+    }
+}   
