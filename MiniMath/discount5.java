6ea2fc538696846d55cd83bf93458a1a6891e278
Cristiano912 <142909694+Cristiano912@users.noreply.github.com>
2023-09-14 14:09:08 +0000

    q

---
+++ MiniMath/discount5.java
@@ -0,0 +1,12 @@
+class discount5
+{
+    public static void main(String args[])
+    {
+        int price = 209;
+       
+        int discount = price / 100 * 20;
+
+
+        System.out.println(price - discount);
+    }
+}   
