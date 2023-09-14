090b11f10be881ff7127c1d828550e61ee994c9a
Cristiano912 <142909694+Cristiano912@users.noreply.github.com>
2023-09-14 13:36:09 +0000

    w

---
+++ MiniMath/add1.java
@@ -0,0 +1,10 @@
+class add1
+{
+    public static void main(String args[])
+    {
+        Byte number1 = 3;
+        Byte number2 = 7;
+
+        System.out.println(number1 + number2);
+    }
+}   
---
+++ MiniMath/birth2.java
@@ -0,0 +1,10 @@
+class add1
+{
+    public static void main(String args[])
+    {
+        short year = 2023;
+        Byte age = 16;
+
+        System.out.println(year - age);
+    }
+}   
