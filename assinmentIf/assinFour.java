1f619af9ee2cf8520180c80749277389f823e7eb
Cristiano912 <142909694+Cristiano912@users.noreply.github.com>
2023-11-08 19:19:17 +0000

    w

---
+++ assinmentIf/assinFour.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+
+class assinFour
+{ 
+  public static void main(String args[])
+  {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.print("Im going to find out if you have selected a leap year. please eneter a number from 1990 to 2023.:");
+    int number = scan.nextInt();
+    if(number == 2020  )
+    {
+      System.out.print(" thats a leap year!");
+    }
+    if(number == 2016  )
+    {
+      System.out.print(" thats a leap year!");
+    }
+    if(number == 2012  )
+    {
+      System.out.print(" thats a leap year!");
+    }
+    if(number == 2008  )
+    {
+      System.out.print(" thats a leap year!");
+    }
+    if(number == 2004  )
+    {
+      System.out.print(" thats a leap year!");
+    }
+    if(number == 2000  )
+    {
+      System.out.print(" thats a leap year!");
+    }
+    if(number == 1996  )
+    {
+      System.out.print(" thats a leap year!");
+    }
+    if(number == 1992  )
+    {
+      System.out.print(" thats a leap year!");
+    }
+    if(number == 1980  )
+    {
+      System.out.print(" thats a leap year!");
+    }
+    if(number >= 2021 && number <=2023)
+    {
+      System.out.print(" Thats not a leap year");
+    }
+    if(number >= 2021 && number <=2023)
+    {
+      System.out.print(" Thats not a leap year");
+    }
+    if(number >= 2017 && number <=2019)
+    {
+      System.out.print(" Thats not a leap year");
+    }
+    if(number >= 2013 && number <=2015)
+    {
+      System.out.print(" Thats not a leap year");
+    }
+    if(number >= 2009 && number <=2011)
+    {
+      System.out.print(" Thats not a leap year");
+    }
+    if(number >= 2005 && number <=2007)
+    {
+      System.out.print(" Thats not a leap year");
+    }
+    if(number >= 2001 && number <=2003)
+    {
+      System.out.print(" Thats not a leap year");
+    }
+    if(number >= 1997 && number <=1999)
+    {
+      System.out.print(" Thats not a leap year");
+    }
+    if(number >= 1993 && number <=1995)
+    {
+      System.out.print(" Thats not a leap year");
+    }
+    if(number >= 1990 && number <=1991)
+    {
+      System.out.print(" Thats not a leap year");
+    }
+  }
+}
---
+++ stringReverse/stringReverb.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class stringReverb
+{ 
+  public static void main(String args[])
+  {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.print("Please enter somthing: ");
+    String name = scan.nextLine();
+    String revstring = "";
+
+    for(int i = name.length() -1; i > -1; i--)
+    {
+      revstring = name.charAt()
+    }
+    int integer=stringNameIndexOf('1');
+    //for(int i=0; i<0; i++)
+    //{
+
+    //}
+    //reverse it
+
+    //print it
+ 
+  }
+}
+
+
