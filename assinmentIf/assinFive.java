48814017a24e6a046d12e699fe9d87128948eac4
Cristiano912 <142909694+Cristiano912@users.noreply.github.com>
2023-11-08 20:34:10 +0000

    woohooo, this was hard, only took me 2 hours

---
+++ assinmentIf/assinFive.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class assinFive 
+{
+  public static void main(String args[]) 
+  {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.print("Please enter a vowel: ");
+    String input = scan.nextLine();
+    
+    if (input.length() == 1) {
+      char charky = input.charAt(0);
+      if (charky == 'a' || charky == 'e' || charky == 'i' || charky == 'o' || charky == 'u')
+      {
+        System.out.println("That's a vowel");
+      } 
+      else 
+      {
+        System.out.println("That's not a vowel");
+      }
+    }
+    else 
+    {
+      System.out.println("Please enter a single character.");
+    }
+  }
+}
+
+
+
