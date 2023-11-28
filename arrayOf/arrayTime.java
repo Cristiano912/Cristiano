a40eb0717c330408115f9b348ac0fb389d3a65c8
Cristiano912 <142909694+Cristiano912@users.noreply.github.com>
2023-11-28 01:24:55 +0000

    w

--- arrayOf/arrayTime.java
+++ arrayOf/arrayTime.java
@@ -4,29 +4,30 @@ class arrayTime
 {
     public static void main(String args[])
     {
-        // Asking the user for the number of entries in their array
+        
         Scanner scan = new Scanner(System.in);
 
         System.out.println("enter a number");
         int num = scan.nextInt();
-        scan.nextLine(); //Do not delete
+        scan.nextLine(); 
 
-        //Initializes the array to be the correct size
         int array [] = new int[num];
       
-        //Reads in user Values
         for(int i = 0; i < num; i++)
         {
             System.out.println("enter a number");
             array[i]=scan.nextInt();
         }
-        System.out.print("the array is");
+        System.out.print("the array is ");
         for(int i = 0; i < num; i++)
         {
-            System.out.print(arrray[i]);
-            if(i < num - 1)
-        }
+        
+            System.out.print(array[i]);
+            if(i < num - 1) 
+            {
 
+            } 
+        }
     }
 }   
 
