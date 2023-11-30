6bf82e9266d59e88e3b24866c65da97bead50755
Cristiano912 <142909694+Cristiano912@users.noreply.github.com>
2023-11-30 14:37:02 +0000

    w

--- arrayDuplicate/duplicate.java
+++ arrayDuplicate/duplicate.java
@@ -8,22 +8,62 @@ class duplicate
 
         System.out.print("Please enter up to 25 values: ");
         int[] values = new int[25];
-        int count = 0;
+        int size = 0;
+        boolean flag = false;
+        int index = -1;
+        int i = 0;
 
-
-        while(count < 25)
+        while(!flag && size < values.length )
         {
-            values[count] = scan.nextInt();
-            count++;
-        }
+            
+            int userInput = scan.nextInt();
 
-        for(int i = 0; i < 25; i++)
-        {
+           
+            values[size] = userInput;
+            i++;
 
-            for(int j = i; j < 25 - i; j++)
+            for(int j = i + 1; j < size; j++ )
+            {
+                
+                if(values[i] == values[j])
+                {
+                    flag = true;
+                    index = i;
+                }
+
             }
-         
+            
+        }
+        if (flag) 
+        {
+            System.out.print("theres a dupe! the first dupe to apear was" + values[index]);
         }
     }
-  
+}
+//Ms. Lindsey
+//Define Scanner
+//Define an array to be size 25
+//Define and itialize int size to be 25
+//Define flag = false, i = 0, and j = 0, and index = -1
+
+//For loop through 25 values allowing the user to enter values and inserting them into the array
+//[whatever they entered 1st][2nd][3rd][etc.]
+
+/*while(!flag && i < size)
+  {
+    for(int j = i; j < size; j++)
+    {
+        if(array[i] == array[j+1])
+        {
+            flag = true;
+            index = i;
+        }
+    }
+    i++
+  }
+*/
+
+// if flag == true -> There is a duplicated
+// else -> no dupe
+
 
