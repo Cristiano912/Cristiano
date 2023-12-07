77481de73906b2c675308117b7725080f1f92072
Cristiano912 <142909694+Cristiano912@users.noreply.github.com>
2023-12-07 04:06:35 +0000

    w

--- loopPractice/stageOne.java
+++ loopPractice/stageOne.java
@@ -3,9 +3,16 @@ class stageOne
 {
   public static void main(String args[])
   {
-    Scanner scan = new Scanner(System.in)
-     System.out.println("enter a numer ");
-     int number = scanner.nextInt()
+    Scanner scan = new Scanner(System.in);
+     System.out.print("enter a number ");
+     int number = scan.nextInt();
     
+    long factorial = 1;
+
+    for(int i = 2; i <= number; i++)
+    {
+      factorial *= i;
+    }
+    System.out.print("factorial of " + number + " is " + factorial);
   }
 }
---
+++ loopPractice/stageTwo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class stageOne
+{
+  public static void main(String args[])
+  {
+    Scanner scan = new Scanner(System.in);
+     System.out.print("enter a word ");
+     String input = scan.nextLine();
+
+     System.out.print("Reveresed ");
+     
+     for(int i = input.length() - 1; i >= 0; i--)
+     {
+        System.out.print(input.charAt(i));
+     }
+
+    
+  }
+}
