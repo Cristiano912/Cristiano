0684010044187b539866a73fd394e1ef640cf4bb
Cristiano912 <142909694+Cristiano912@users.noreply.github.com>
2023-12-08 05:17:12 +0000

    w

--- scanMinnies/mini4.java
+++ scanMinnies/mini4.java
@@ -1,18 +1,27 @@
 import java.util.Scanner;
-class stageFour
+class mini4
 {
   public static void main(String args[])
   {
         Scanner scan = new Scanner(System.in);
          
-        System.out.print("enter a number ");
-        int num1 = scan.nextInt();
+        System.out.print("enter a tempeture ");
+        int tempeture = scan.nextInt();
+       
+        System.out.print("enter c to convert to Celcius and f to covert to Fehrenheit ");
+       char unit = scan.next().charAt(0);
 
-        System.out.print("enter a number ");
-        int num2 = scan.nextInt();
+       int convertedTempeture = 0;
 
-        int sum = num1 + num2;
-       System.out.print(sum ); 
-
+       if(unit == 'c')
+       {
+        convertedTempeture = (tempeture * 9/5) + 32;
+        System.out.print("tempeture in fehrenheit " + convertedTempeture);
+       }
+       else if(unit == 'f')
+       {
+        convertedTempeture = (tempeture - 32) * 5 / 9;
+        System.out.print("tempeture in Celsius" + convertedTempeture);
+       }
   }
 }
